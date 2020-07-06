de5859c616666193fcf5af154905d0eecd3ab477
projetosm <67690393+projetosm@users.noreply.github.com>
2020-07-06 10:33:17 +0100

    Add files via upload
    Adcionei os script js para fazer o efeito de slide do form multistep.

--- js/script.js
+++ js/script.js
@@ -17,3 +17,31 @@
  	});
  });
 
+
+const slidepaginas = document.querySelector('.slidepaginas');
+
+
+const primeiroNextBtn = document.querySelector('.nxtBtn');
+const segundoPrevBtn = document.querySelector('.prev-1');
+const segundoNextBtn = document.querySelector('.next-1');
+const terceiroPrevBtn = document.querySelector('.prev-2');
+const terceiroNextBtn = document.querySelector('.next-2');
+const quartoPrevBtn = document.querySelector('.prev-3');
+const submitBtn = document.querySelector('.btn-salvar');
+
+const fazslidefuncao = (botao, margin) => {
+		botao.addEventListener("click", ()=>{
+		slidepaginas.style.marginLeft= margin+"%";
+	});
+}
+
+fazslidefuncao(primeiroNextBtn, -21.7);
+fazslidefuncao(segundoPrevBtn, 0);
+fazslidefuncao(segundoNextBtn, -43.4);
+fazslidefuncao(terceiroPrevBtn, -21.7);
+fazslidefuncao(terceiroNextBtn, -65.1);
+fazslidefuncao(quartoPrevBtn, -43.4);
+
+submitBtn.addEventListener("click", ()=>{
+		alert('registrado com sucesso');
+	});
